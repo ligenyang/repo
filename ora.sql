c8256493b629c30001c52816f9969a6509a2f5db
ligenyang <41351858+ligenyang@users.noreply.github.com>
2018-07-18 15:32:05 +0800

    Update ora.sql

--- ora.sql
+++ ora.sql
@@ -1,3 +1,20 @@
 select s.username, l.object_id, l.session_id, s.serial#, s.lockwait, s.status, s.machine, s.program from v$session s, v$locked_object l where s.sid = l.session_id;
 select sql_text from v$sql where hash_value in (select sql_hash_value from v$session where sid in (select session_id from v$locked_object));
 alter system kill session '68,24231'; --SID,serial#
+
+
+with
+    TB_CHSS_GRJBXX_ as ( 
+      select
+      t.yljgdm, 
+      t.JDYSGH, 
+      t.JDYSXM 
+    from TB_CHSS_GRJBXX@yanhe t group by t.JDYSGH, t.JDYSXM, t.yljgdm
+),
+    GDWS_SYS_USER_ as ( 
+      select t.INS_ID, 
+        t.USER_NAME, 
+        t.REAL_NAME 
+      from GDWS_SYS_USER t where t.AREA_ID like '520624%' group by t.USER_NAME, t.REAL_NAME, t.INS_ID
+  )
+select a.*, b.* from TB_CHSS_GRJBXX_ a join GDWS_SYS_USER_ b on a.JDYSXM = b.REAL_NAME; 
