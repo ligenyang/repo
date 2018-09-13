fa8bd3a4e1ce1a96efab32342ff2ea7df8e6cee9
ligenyang <41351858+ligenyang@users.noreply.github.com>
2018-09-13 14:47:41 +0800

    Update java.java

--- java.java
+++ java.java
@@ -1,1 +1,6 @@
 listObj.removeAll(Collections.singleton(null)); // 删除明细对象字段全为null的数据
+
+-- 环境变量
+JAVA_HOME C:\Program Files\Java\jdk1.8.0_162
+CLASSPATH .;%JAVA_HOME%\lib\dt.jar;%JAVA_HOME%\lib\tools.jar;
+Path %JAVA_HOME%\bin;%JAVA_HOME%\jre\bin;
