c0f45a0567e889abff53ec37777e694b18d8a58b
ligenyang <41351858+ligenyang@users.noreply.github.com>
2019-02-28 16:10:39 +0800

    Delete JavaScript.js

--- JavaScript.js
+++
@@ -1,4 +0,0 @@
-const date = new Date(), y = date.getFullYear(), m = date.getMonth();
-const currentDay = date.formatDate("yyyy-MM-dd");//当前日期
-const firstDay = new Date(y, m, 1).formatDate('yyyy-MM-dd');
-const lastDay = new Date(y, m + 1, 0).formatDate('yyyy-MM-dd');//上个月的今天
